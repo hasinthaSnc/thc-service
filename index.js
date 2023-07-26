c3c8a3619238f48a7648cb9d8f24215e1ba5a1c9
hasinthaSnc <139563894+hasinthaSnc@users.noreply.github.com>
2023-07-26 13:20:46 +0530

    condition fixes

--- index.js
+++ index.js
@@ -304,7 +304,7 @@ app.get("/email", async (req, res) => {
 });
 
 app.get("/", async (req, res) => {
- return res.json("THE THC RUNNING FINE V2.1")
+ return res.json("THE THC RUNNING FINE V2.2")
 });
 
 app.use("/lad-collective", invoiceRoute);

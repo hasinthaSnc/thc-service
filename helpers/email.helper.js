bf80965127675c3c24d8fd1044f0ed56628683da
hasinthaSnc <139563894+hasinthaSnc@users.noreply.github.com>
2024-03-28 10:55:51 +0530

    save changes

--- helpers/email.helper.js
+++ helpers/email.helper.js
@@ -94,26 +94,25 @@ const sendAmazonAlertEmail = async (orderId,orderNumber, message) => {
 
 const sendInquiryEmailForCustomer = async (customerEmail) => {
   try {
+    const htmlContent = `<p>G’day,</p>
+    <p>Thank you for reaching out to The Lad Collective. We wanted to let you know that we've successfully received your email.
+    </p>
+    <p>We are currently out of office and will return on Tuesday the 2nd of April. Your message is very important to us and we will do our best to get back to you as soon as we can once we have returned from the short break.</p>
+    <p>On behalf of the TLC Team, we hope you have an amazing Easter weekend.</p>
+    <p>Talk soon :blush::rabbit:</p>
+    <br>
+    <p>Warm regards,<br>
+    The Lad Collective Team</p>`;
+
     // const htmlContent = `<p>G’day,</p>
     // <p>Thank you for reaching out to The Lad Collective. We wanted to let you know that we've successfully received your email.
     // </p>
-    // <p>We are currently experiencing a high volume of enquiries and are working diligently to respond to each one as quickly and thoroughly as possible. Our commitment to providing you with the best service remains our top priority.</p>
+    // <p>We are currently experiencing a high volume of enquiries and are working diligently to respond to each one quickly. Our commitment to providing you with the best service remains our top priority.</p>
     // <p>We appreciate your patience and understanding during this busy time. Rest assured, a member of our customer service team will be in touch with you as soon as we can.</p>
-    // <p>If your query is urgent, please don't hesitate to reach out to our customer care team on 0427 031 620.</p>
     // <p>Thank you for choosing The Lad Collective. We're looking forward to assisting you!</p>
     // <br>
     // <p>Warm regards,<br>
     // The Lad Collective Team</p>`;
-
-    const htmlContent = `<p>G’day,</p>
-    <p>Thank you for reaching out to The Lad Collective. We wanted to let you know that we've successfully received your email.
-    </p>
-    <p>We are currently experiencing a high volume of enquiries and are working diligently to respond to each one quickly. Our commitment to providing you with the best service remains our top priority.</p>
-    <p>We appreciate your patience and understanding during this busy time. Rest assured, a member of our customer service team will be in touch with you as soon as we can.</p>
-    <p>Thank you for choosing The Lad Collective. We're looking forward to assisting you!</p>
-    <br>
-    <p>Warm regards,<br>
-    The Lad Collective Team</p>`;
 
     let transporter = nodemailer.createTransport({
       service: "gmail",
